4635f8ab5b25a89cc01b6757d45d917604d3f2c6
Gaurav Dogra <gauru.dogra@gmail.com>
2021-08-06 02:10:28 +0100

    used spring initializer with spring web dependency to create a maven project

---
+++ src/main/java/com/gmail/gauru/dogra/easynotifications/EasyNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.gauru.dogra.easynotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyNotificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/gauru/dogra/easynotifications/EasyNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.gauru.dogra.easynotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
