eec52e9ef8434cb173187fd146abc5eedb1ac3dc
Gaurav Dogra <gauru.dogra@gmail.com>
2021-08-14 02:16:10 +0100

    disabled csrf, and enabled all end point temporarily

--- src/main/java/com/gmail/gauru/dogra/easynotifications/security/WebSecurityConfig.java
+++ src/main/java/com/gmail/gauru/dogra/easynotifications/security/WebSecurityConfig.java
@@ -18,15 +18,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
+//                .antMatchers("/", "/home").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+                .anyRequest()
                 .permitAll();
+
+        http
+                .csrf().disable();
     }
 
     @Bean
