03a30c86e558ea1778cd8e259a5deaf023aed005
Gaurav Dogra <gauru.dogra@gmail.com>
2021-09-19 02:55:19 +0100

    UserServiceImpl now implements UserDetailsService as well

--- src/main/java/com/gmail/gauru/dogra/easynotifications/service/UserServiceImpl.java
+++ src/main/java/com/gmail/gauru/dogra/easynotifications/service/UserServiceImpl.java
@@ -6,21 +6,30 @@ import com.gmail.gauru.dogra.easynotifications.repo.RoleRepo;
 import com.gmail.gauru.dogra.easynotifications.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the db", user.getUserName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user); // validation pending
     }
 
@@ -31,7 +40,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void saveRoleToUser(String userName, String roleName) {
+    public void addRoleToUser(String userName, String roleName) {
         // validations pending
         log.info("saving role {} to user {}", roleName, userName);
         User user = userRepo.findByUserName(userName);
@@ -49,5 +58,20 @@ public class UserServiceImpl implements UserService {
     public List<User> getUsers() {
         log.info("fetching all users");
         return userRepo.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepo.findByUserName(userName);
+        if (user == null) {
+            log.error("User {} not found in the db", userName);
+            throw new UsernameNotFoundException("User not found in the db");
+        } else {
+            log.info("user {} found in the db", userName);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role ->
+                authorities.add(new SimpleGrantedAuthority(role.getRoleName())));
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), authorities);
     }
 }
