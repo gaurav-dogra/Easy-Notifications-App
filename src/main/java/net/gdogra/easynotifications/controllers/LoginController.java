af7b11ae09889efb1565d98c3b99b0bc2762966b
Gaurav Dogra <gauru.dogra@gmail.com>
2021-10-04 20:21:57 +0100

    added a new pic for login cum signup form

--- src/main/java/net/gdogra/easynotifications/controllers/LoginController.java
+++ src/main/java/net/gdogra/easynotifications/controllers/LoginController.java
@@ -4,18 +4,25 @@ import net.gdogra.easynotifications.appuser.AppUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
     @GetMapping("")
     public String login() {
-        return "login";
+        return "signup_login";
     }
 
-    @GetMapping("/signup")
+    @PostMapping("/signup")
     public String signupNewUser(Model model) {
         model.addAttribute("user", new AppUser());
-        return "signup_form";
+        return "signup_successful";
+    }
+
+    @PostMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("user", new AppUser());
+        return "login_successful";
     }
 }
