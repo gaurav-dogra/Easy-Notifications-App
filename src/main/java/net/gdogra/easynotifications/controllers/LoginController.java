be94810f853fd22aa9c5347b9a7132403ea20a54
Gaurav Dogra <gauru.dogra@gmail.com>
2021-10-11 05:36:12 +0100

    handing register and login dtos, form error handing module in signupNewUser()

--- src/main/java/net/gdogra/easynotifications/controllers/LoginController.java
+++ src/main/java/net/gdogra/easynotifications/controllers/LoginController.java
@@ -1,28 +1,49 @@
 package net.gdogra.easynotifications.controllers;
 
-import net.gdogra.easynotifications.appuser.AppUser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.gdogra.easynotifications.dtos.LoginDto;
+import net.gdogra.easynotifications.appuser.AppUserService;
+import net.gdogra.easynotifications.dtos.RegisterDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
+@Slf4j
+@RequiredArgsConstructor
 public class LoginController {
 
-    @GetMapping("")
-    public String login() {
-        return "signup_login";
+    private final AppUserService appUserService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("loginDto", new LoginDto());
+        model.addAttribute("registerDto", new RegisterDto());
+        return "register_login";
     }
 
-    @PostMapping("/signup")
-    public String signupNewUser(Model model) {
-        model.addAttribute("user", new AppUser());
-        return "signup_successful";
+    @PostMapping(value = "/")
+    public String signupNewUser(@Valid @ModelAttribute("dto") LoginDto dto,
+                                BindingResult result) {
+        log.info("SIGNUP");
+        if (result.hasErrors()) {
+            return "register_login";
+        } else {
+            return "success";
+        }
     }
 
-    @PostMapping("/login")
-    public String login(Model model) {
-        model.addAttribute("user", new AppUser());
-        return "login_successful";
-    }
+//    @PostMapping(value = "/", params = "action=login")
+//    public String login(@Valid AppUserDto dto) {
+//        log.info("LOGIN");
+//        log.info(dto.getEmail());
+//        log.info(dto.getPassword());
+//        return "success";
+//    }
 }
